2a645de2e2b25168185107313d7a35496b230b2e
Hari Krishna <bkrishna.hari@gmail.com>
2017-12-21 17:42:54 +0530

    Added automation cert changes

--- Azure Automation For Data Manager/Trigger-DataTransformation-Job.ps1
+++ Azure Automation For Data Manager/Trigger-DataTransformation-Job.ps1
@@ -1,37 +1,20 @@
 <#
 .DESCRIPTION
     This runbook triggers the Data Transformation Job. 
-    This runbook depends on C# dlls which handles the Data transformation calls.  
+    This runbook depends on C# dlls which handles the Data transformation calls.
      
-.ASSETS     
-    SubscriptionId: The Subscription Id of the Azure. 
-    TenantId: The Tenant Id (guid) of the Azure Active Directory (AAD) tenant where the service principal resides.
-    ClientId: The Client Id of AAD app which has permissions to KeyVault
-    ActiveDirectoryKey: Client Secret of AAD app which has permissions to KeyVault
+.ASSETS
     ResourceGroupName: The Resource group name of the DMS
     DataManagerName: The name of the DataManager Resource within the specified resource group.
     JobDefinitionName: The name of the Job definition.
 
 .NOTES:
-
 #>
 
 workflow Trigger-DataTransformation-Job
 {
     param
     (
-        [Parameter(Mandatory=$true)]
-        [string]$SubscriptionId,
-
-        [Parameter(Mandatory=$true)]
-        [string]$TenantId,
-
-        [Parameter(Mandatory=$true)]
-        [string]$ClientId,
-
-        [Parameter(Mandatory=$true)]
-        [string]$ActiveDirectoryKey,
-
         [Parameter(Mandatory=$true)]
         [string]$ResourceGroupName,
 
@@ -40,34 +23,52 @@ workflow Trigger-DataTransformation-Job
 
         [Parameter(Mandatory=$true)]
         [string]$JobDefinitionName
-    ) 
-    
-    Write-Output "Job initiated"
-    #ls C:\Modules\User\DataTransformationApp
-    
-    $jobParams = @{
-        SubscriptionId = $SubscriptionId;
-        TenantId = $TenantId;
-        ApplicationId = $ClientId;
-        ActiveDirectoryKey = $ActiveDirectoryKey;
-        ResourceGroupName = $ResourceGroupName;
-        ResourceName = $DataManagerName;
-        JobDefinitionName = $JobDefinitionName
+    )
+
+    $ClientCertificate = Get-AutomationCertificate -Name "AzureRunAsCertificate"
+    if ($ClientCertificate -eq $null) {
+         throw "The AzureRunAsCertificate asset has not been created in the Automation service."
     }
 
-    InlineScript {
-        $jobParams = $Using:jobParams
-        Write-Output "Input params: "
-        Write-Output $jobParams 
+    $ServicePrincipalConnection = Get-AutomationConnection -Name "AzureRunAsConnection"
+    if ($ServicePrincipalConnection -eq $null) {
+         throw "The AzureRunAsConnection asset has not been created in the Automation service."
+    }
 
-        # Load all dependent dlls
-        $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\Newtonsoft.Json.dll")
-        $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\DataTransformationApp.dll")
-        $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\Microsoft.Rest.ClientRuntime.dll")
-        $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\Microsoft.Rest.ClientRuntime.Azure.Authentication.dll")
-        $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\Microsoft.IdentityModel.Clients.ActiveDirectory.dll") 
+    # Get the SubscriptionId, TenantId & ApplicationId
+    $SubscriptionId = $ServicePrincipalConnection.SubscriptionId
+    $TenantId = $ServicePrincipalConnection.TenantId
+    $ClientId = $ServicePrincipalConnection.ApplicationId
+    
+    Write-Output "Initiating to trigger the job"
+    #ls C:\Modules\User\DataTransformationApp
 
-        # Trigger Job definition
-        [DataTransformationApp.DataTransformationApp]::RunJob($jobParams)
+    InlineScript
+    {
+        try {
+            # Load all dependent dlls
+            $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\Newtonsoft.Json.dll")
+            $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\DataTransformationApp.dll")
+            $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\Microsoft.Rest.ClientRuntime.dll")
+            $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\Microsoft.Rest.ClientRuntime.Azure.Authentication.dll")
+            $data = [Reflection.Assembly]::LoadFile("C:\Modules\User\DataTransformationApp\Microsoft.IdentityModel.Clients.ActiveDirectory.dll")
+
+            # Instantiate clientAssertionCertificate
+            $ClientAssertionCert = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.ClientAssertionCertificate -ArgumentList $Using:ClientId, $Using:ClientCertificate
+
+            # Set ConfigurationParams
+            $Configparams = New-Object DataTransformationApp.ConfigurationParams
+            $Configparams.ClientId = $Using:ClientId
+            $Configparams.ResourceGroupName = $Using:ResourceGroupName
+            $Configparams.ResourceName = $Using:DataManagerName
+            $Configparams.SubscriptionId = $Using:SubscriptionId
+            $Configparams.TenantId = $Using:TenantId
+            $Configparams.Certificate = $ClientAssertionCert
+
+            # Trigger Job definition
+            [DataTransformationApp.DataTransformationApp]::RunJob($Configparams, $Using:JobDefinitionName)
+        } catch {
+            throw $_.Exception
+        }
     }
 }
